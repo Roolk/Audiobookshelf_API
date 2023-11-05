0bd0a3dae4ac6227dbcf2b94456836fd7bfe0810
Roolk <31905487+Roolk@users.noreply.github.com>
2023-11-04 19:35:54 -0700

    Update main for testing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from audiobookshelfapi import *
 import json
 import audiobookshelfenums
 
-APITOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3YzFjMDVmNy05MjI4LTRkN2YtODgzMC02OTg3OWRhMTljM2QiLCJ1c2VybmFtZSI6Im5pa28iLCJpYXQiOjE2OTkxNDE3Mjd9.fxjAvBS0xJmQ34A_Xtvb-PJJ2aj7DWzNez4qQF68W_g'
+APITOKEN = ''
 
 if __name__ == '__main__':
     a = Audiobookshelf_API("http://127.0.0.1:13378", APITOKEN)
