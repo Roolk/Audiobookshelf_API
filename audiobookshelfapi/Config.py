906c4ae5c957e65c08b4d327e55fb1158956d1fc
Roolk <31905487+Roolk@users.noreply.github.com>
2024-05-17 22:53:12 -0700

    Added time to start and end

--- audiobookshelfapi/Config.py
+++ audiobookshelfapi/Config.py
@@ -3,3 +3,12 @@ URL = ""
 
 # API token, found in the Audiobookshelf server's settings > Users > *username* > API token
 APIToken = ""
+
+# Library name: The name of the library to act on
+LibraryName = ""
+
+# Time to start encoding (24 Hour)
+StartTime = 22
+
+# Time to end new encoding requests. (24 Hour)
+EndTime = 6
