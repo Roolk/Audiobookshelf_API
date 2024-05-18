9e5ff5337320ef45f19595fe8f7e852475d0d339
Roolk <31905487+Roolk@users.noreply.github.com>
2024-05-17 23:01:03 -0700

    Added number of books to encode and time between checks

--- audiobookshelfapi/Config.py
+++ audiobookshelfapi/Config.py
@@ -7,8 +7,14 @@ APIToken = ""
 # Library name: The name of the library to act on
 LibraryName = ""
 
-# Time to start encoding (24 Hour)
+# Time to start encoding (24 Hour). These defaults will encode from 10pm to 6am.
 StartTime = 22
 
 # Time to end new encoding requests. (24 Hour)
 EndTime = 6
+
+# The number of books to simultaneously encode
+Number_of_books_to_encode = 2
+
+# Time between requests to the server to check if the book has finished converting in seconds
+TIME_BETWEEN_CHECKS = 60
